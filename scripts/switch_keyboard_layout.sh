65022026da64917b3132921785bf2770446c7333
Brian Ferri <me@brianferri.dev>
2025-10-11 14:30:19 +0200

    initial commit

---
+++ scripts/switch_keyboard_layout.sh
@@ -0,0 +1,4 @@
+hyprctl switchxkblayout all next
+
+layMain=$(hyprctl -j devices | jq '.keyboards' | jq '.[] | select (.main == true)' | awk -F '"' '{if ($2=="active_keymap") print $4}')
+notify-send -a "Keyboard" -r 91190 -t 800 "${layMain}"
